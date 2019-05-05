bf3f4f657b2ae7ef211bbc3e77ae4b9e32393dd8
senthil338 <48490092+senthil338@users.noreply.github.com>
2019-05-05 15:28:55 -0400

    Added find method with loop

--- BloomFilter/BloomFiltersln/BloomFiltersln/Program.cs
+++ BloomFilter/BloomFiltersln/BloomFiltersln/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +14,7 @@ namespace BloomFiltersln
         static void Main(string[] args)
         {
             // fp_probability : false positive probability should be between 0.1 and 0.9
+            //m number of items to be constructed 
             BloomFilter bloomfilter = new BloomFilter(m: 200, fp_probability: 0.055);
             string[] text = {
                 "Australia", "Belgium", "Brazil", "Canada",
@@ -26,11 +27,29 @@ namespace BloomFiltersln
             {
                 bloomfilter.Add(text[i]);
             }
-            int cnt = bloomfilter.Count();
-            // return true if value found in the bloom filter otherwise false
-            // values are case sensitive
-            bool isFound1 = bloomfilter.Contains("Brazil"); 
-            bool isFound2 = bloomfilter.Contains("Japan");
+            
+
+            string[] find = {
+                "Australia", "Bahrain","Belgium", "Colombia","Brazil", "Canada",
+                "China", "Denmark", "Germany", "Jamaica",
+                "India", "Madagascar" ,"SriLanka", "Kuwait",
+                "Afghanistan", "Argentina", "The Bahamas", "Costa Rica","Luxembourg",
+                "Iceland", "Bhutan", "Hungary","Sweden","Japan"
+            };
+
+            for(int i=0;i< find.Length; i++)
+            {
+                if(bloomfilter.Contains(find[i]))
+                {
+                    Console.WriteLine("Bloom Filter contain {0}", find[i]);
+                }
+                else
+                {
+                    Console.WriteLine("Bloom Fiter not contain {0}", find[i]);
+                }
+            }
+
+            Console.ReadLine();
             
 
         }
